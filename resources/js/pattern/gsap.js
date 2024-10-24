3bfb7f249c15d24663c8d8f3e1a6e2db2aab1d73
yejiahn1 <yeji_09@naver.com>
2024-10-24 11:34:49 +0900

    index html

--- resources/js/pattern/gsap.js
+++ resources/js/pattern/gsap.js
@@ -27,17 +27,25 @@ window.addEventListener("scroll",_.throttle(
         duration :1 
     
       });
+      gsap.to("#to-top", {
+        x:-70,
+        duration:0.3
+      });
     }else{
       gsap.to(".inner",{
         y : 0,
         duration :1 
     
       });
+      gsap.to("#to-top", {
+        x:0,
+        duration:0.3
+      });
     }
 
 
   }
-
+  
 
 
 )); 
@@ -46,15 +54,9 @@ window.addEventListener("scroll",_.throttle(
 
 
 
-gsap.from(".title", {
-  scrollTrigger: {
-    trigger: ".title",  // 애니메이션을 트리거할 요소
-    start: "top 30%",     // 애니메이션 시작 위치
-    end: "bottom",       // 애니메이션 종료 위치
-    scrub: true,          // 스크롤과 애니메이션을 동기화
-    markers: true          // 디버깅용 마커 표시 (개발 중에만 사용)
-  },
-           // 애니메이션 속성
-  y: -260,                 // y축 이동
-  duration: 1            // 애니메이션 지속 시간
+gsap.to(".title,.txt",{
+  scrollTrigger : ".title",
+  x: 300,
+  y:-200,
+  duration: 2
 });
