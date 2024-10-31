5f3c2bba7984b34912c5477dc82802c903e22e38
yejiahn1 <yeji_09@naver.com>
2024-10-31 16:49:49 +0900

    sub page

--- resources/js/pattern/ui-pattern-script.js
+++ resources/js/pattern/ui-pattern-script.js
@@ -1,43 +1,14 @@
 AOS.init();
-// .all-menu 를 클릭했을 때
-// #nav-all 에게 .active 클래스를 추가한다.
-
+/* 햄버거 메뉴 보이게 */
 $(".all-menu").click(function () {
-  $("#nav-all .inner").addClass("active");
+  $("#nav-all").addClass("active");
 });
 
-document.querySelector(".all-menu").addEventListener("click", () => {
-  const navAll = document.querySelector("#nav-all");
-  navAll.classList.add("active");
-});
-
-// #nav-all .close 를 클릭했을 때
-// #nav-all 에게 .active 클래스를 제거한다.
-$("#nav-all > .inner> .close").click(function () {
+$(".close").click(function () {
   $("#nav-all").removeClass("active");
 });
 
-$("#gnb").mouseenter(function () {
-  $("#header").addClass("active");
-});
-
-$("#gnb").mouseleave(function () {
-  $("#header").removeClass("active");
-});
-
-$(".lang__btn").click(function () {
-  $(".lang__lst").show();
-});
-
-$(".lang__lst li").click(function () {
-  $(".lang__lst").hide();
-});
-
-$(".lang__btn").mouseleave(function () {
-  $(".lang__lst").hide();
-});
-
-//#to-top 눌렀을 때 상단으로 이동하기
+/* 햄버거 메뉴 메인메뉴 클릭시 하단 메뉴 보이게 */
 $("#nav-all > .inner > .gnb__depth1 > li ").mouseenter(function () {
   $(this).find(".gnb__depth2").stop().slideDown(200);
 });
@@ -45,7 +16,7 @@ $("#nav-all > .inner > .gnb__depth1 > li ").mouseenter(function () {
 $("#nav-all > .inner > .gnb__depth1 > li ").mouseleave(function () {
   $(this).find(".gnb__depth2").stop().slideUp(200);
 });
-
+/* 검색창 */
 $(".util > .search").click(function () {
   $(".modal-search").css({
     top: "0",
@@ -58,4 +29,13 @@ $(".close").click(function () {
   });
 });
 
-/* include 스크립트 적용 안됨*/
+$(".lang__btn").click(function () {
+  $(".lang__lst").show();
+});
+
+$(".lang__lst li").click(function () {
+  $(".lang__lst").hide();
+});
+$(".lang__lst").mouseleave(function () {
+  $(this).hide();
+});
