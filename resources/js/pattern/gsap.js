6e3cefa0b672a79aa9eb51a9f1b7f51498df66be
yejiahn1 <yeji_09@naver.com>
2025-01-02 16:43:01 +0900

    gsap 모바일

--- resources/js/pattern/gsap.js
+++ resources/js/pattern/gsap.js
@@ -4,43 +4,47 @@ gsap.registerPlugin(ScrollTrigger);
 document.addEventListener("DOMContentLoaded", function () {
   // 약간의 지연을 주어 DOM이 완전히 준비되도록 함
   setTimeout(() => {
-    const animations = [
-      {
-        element: ".section-contentInner",
-        trigger: ".section-contentInner",
-        start: "top 80%",
-        end: "bottom 60%",
-      },
-      {
-        element: ".title2",
-        trigger: ".txt__price",
-        start: "top top",
-        end: "bottom bottom",
-      },
-      {
-        element: ".txt2",
-        trigger: ".section-img2",
-        start: "top top",
-        end: "top bottom",
-      },
-    ];
+    // 데스크톱에서만 애니메이션 실행
+    const mm = gsap.matchMedia();
 
-    animations.forEach(({ element, trigger, start, end }) => {
-      gsap.from(element, {
-        scrollTrigger: {
-          trigger: trigger,
-          start: start,
-          end: end,
-          scrub: 1,
-          markers: false, // 프로덕션에서는 false로 설정
-          // 모바일에서의 성능 최적화를 위한 옵션
-          toggleActions: "play none none reverse",
-          fastScrollEnd: true,
+    mm.add("(min-width: 768px)", () => {
+      const animations = [
+        {
+          element: ".section-contentInner",
+          trigger: ".section-contentInner",
+          start: "top 80%",
+          end: "bottom 60%",
         },
-        y: 70,
-        opacity: 0,
-        ease: "power2.out",
-        duration: 1,
+        {
+          element: ".title2",
+          trigger: ".txt__price",
+          start: "top top",
+          end: "bottom bottom",
+        },
+        {
+          element: ".txt2",
+          trigger: ".section-img2",
+          start: "top top",
+          end: "top bottom",
+        },
+      ];
+
+      animations.forEach(({ element, trigger, start, end }) => {
+        gsap.from(element, {
+          scrollTrigger: {
+            trigger: trigger,
+            start: start,
+            end: end,
+            scrub: 1,
+            markers: false,
+            toggleActions: "play none none reverse",
+            fastScrollEnd: true,
+          },
+          y: 70,
+          opacity: 0,
+          ease: "power2.out",
+          duration: 1,
+        });
       });
     });
   }, 100);
